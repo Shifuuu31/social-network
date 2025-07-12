205663b63ddd959f2c5438350a03bc5056b1ee24
Aymane Saadane <ayman.saadane@gmail.com>
2025-07-12 05:50:19 +0100

    Update and rename

--- backend/pkg/db/migrations/sqlite/000005_privacy_selected_table.up.sql
+++ backend/pkg/db/migrations/sqlite/000005_privacy_selected_table.up.sql
@@ -1,11 +1,11 @@
 -- Migration: Create post_privacy_selected table
 -- Links posts with specific users allowed to see posts marked 'selected'
 
-CREATE TABLE IF NOT EXISTS post_privacy_selected (
+CREATE TABLE IF NOT EXISTS privacy_selected (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    post_id INTEGER NOT NULL,
     user_id INTEGER NOT NULL,
+    selected_user_id INTEGER NOT NULL,
     UNIQUE(post_id, user_id),
-    FOREIGN KEY(post_id) REFERENCES posts(id) ON DELETE CASCADE,
     FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE
+    FOREIGN KEY(selected_user_id) REFERENCES users(id) ON DELETE CASCADE,
 );
